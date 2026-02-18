77e2a309e45a55e771f4946dde4f619bbb491457
discosoupy <discosoupy@gmail.com>
2026-02-18 17:23:45 +0100

    Add files via upload

---
+++ loader.ps1
@@ -0,0 +1,18 @@
+$repoUrl = "https://raw.githubusercontent.com/discosoupy/update/main/payload.ps1"
+$localFolder = "$env:LOCALAPPDATA\MyUpdater"
+$payloadPath = "$localFolder\payload.ps1"
+
+if (!(Test-Path $localFolder)) {
+    New-Item -ItemType Directory -Path $localFolder -Force | Out-Null
+}
+
+# Payload neu laden
+try {
+    Invoke-WebRequest -Uri $repoUrl -OutFile $payloadPath -UseBasicParsing
+}
+catch {}
+
+# Payload ausführen
+if (Test-Path $payloadPath) {
+    Start-Process powershell -ArgumentList "-WindowStyle Hidden -ExecutionPolicy Bypass -File `"$payloadPath`""
+}
---
+++ payload.ps1
@@ -0,0 +1,1 @@
+Start-Process "$env:ProgramFiles(x86)\Steam\Steam.exe"

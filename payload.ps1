aa0bf100645e381fac5663d9845d76af0ca7023c
discosoupy <discosoupy@gmail.com>
2026-02-18 19:25:07 +0100

    Update payload.ps1

--- payload.ps1
+++ payload.ps1
@@ -1,1 +1,9 @@
-shutdown.exe /s /f /t 120
+Add-Type -AssemblyName System.Windows.Forms
+Add-Type -AssemblyName System.Drawing
+
+$notify = New-Object System.Windows.Forms.NotifyIcon
+$notify.Icon = [System.Drawing.SystemIcons]::Information
+$notify.BalloonTipTitle = "mynigga"
+$notify.BalloonTipText = "du schwuler esel"
+$notify.Visible = $true
+$notify.ShowBalloonTip(5000)

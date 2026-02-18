f364f313cf9534dd685bfb71689507bbc97cfb23
discosoupy <discosoupy@gmail.com>
2026-02-18 15:00:33 +0100

    Update update.ps1

--- update.ps1
+++ update.ps1
@@ -1,12 +1,18 @@
-$repoUrl = "https://raw.githubusercontent.com/USERNAME/REPO/main/update.ps1"
-$localPath = "$env:LOCALAPPDATA\MyUpdater\update.ps1"
+$repoUrl = "https://raw.githubusercontent.com/discosoupy/update/refs/heads/main/update.ps1"
+$localFolder = "$env:LOCALAPPDATA\MyUpdater"
+$localPath = "$localFolder\update.ps1"
+
 
 try {
+    if (!(Test-Path $localFolder)) {
+        New-Item -ItemType Directory -Path $localFolder -Force | Out-Null
+    }
+
     Invoke-WebRequest -Uri $repoUrl -OutFile $localPath -UseBasicParsing
-} catch {
-    # Falls Download fehlschlägt, alten Code weiter benutzen
+}
+catch {
+   
 }
 
-# ====== DEIN EIGENTLICHER CODE HIER ======
-# Beispiel:
+
 Start-Process "notepad.exe"
